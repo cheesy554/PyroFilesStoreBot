5d6e0e056326b7818a28af6ed62dec78ed474af7
Nasurancreation <88917315+Nasurancreation@users.noreply.github.com>
2022-01-16 08:48:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -158,7 +158,7 @@ async def start(bot, cmd):
 		try:
 			file_id = int(usr_cmd)
 			send_stored_file = await bot.copy_message(chat_id=cmd.from_user.id, from_chat_id=DB_CHANNEL, message_id=file_id)
-			await send_stored_file.reply_text(f"**Here is Sharable Link of this file:** https://telegram.dog/{BOT_USERNAME}?start=ITSOKTV_{file_id}\n\n__To Retrive the Stored File, just open the link!__", disable_web_page_preview=True, quote=True)
+			await send_stored_file.reply_text(f"**Hᴇʀᴇ ɪs Sʜᴀʀᴀʙʟᴇ Lɪɴᴋ ᴏғ ᴛʜɪs ғɪʟᴇ:** https://telegram.dog/{BOT_USERNAME}?start=ITSOKTV_{file_id}\n\n__To Retrive the Stored File, just open the link!__", disable_web_page_preview=True, quote=True)
 		except Exception as err:
 			await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
 
