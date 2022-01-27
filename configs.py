138e9c6bf4eb7d0d3632066bc6fc4f2c1b0f43bb
cheesy554 <72846161+cheesy554@users.noreply.github.com>
2022-01-27 21:41:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,6 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-        FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", TRUE))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
